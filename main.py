965928787ba976a271d5cdfb96bf4846bbb6ff87
Mitchell Kealy <60372669+mitchellkealy@users.noreply.github.com>
2022-01-25 17:51:53 +0000

    Removed unnecessary gates

--- main.py
+++ main.py
@@ -1,32 +1,7 @@
-def gate_and(input1, input2):
-  ''' an AND gate (input1 ^ input2)'''
-  gate1 = input1 and input2
-  return bool(gate1)
-
-def gate_or(input1, input2):
-  '''an OR gate (input1 v input2)'''
-  gate1 = input1 or input2
-  return bool(gate1)
-
-def gate_xor(input1, input2):
-  '''a XOR gate ( (input1 ^ -input2) v (-input1 ^ input2))'''
-  gate1 = input1 and not input2
-  gate2 = input2 and not input1
-  return bool(gate1 or gate2)
-
 def gate_nand(input1, input2):
   '''a NAND gate (-(input1 ^ input2)) '''
   gate1 = not(input1 and input2)
   return bool(gate1)
-
-def gate_nor(input1, input2):
-  '''a NOR gate (-(input1 v input2)) '''
-  gate1 = not(input1 or input2)
-  return bool(gate1)
-
-def gate_not(input1):
-  '''a NOT gate (-input1)'''
-  return bool(not input1)
 
 print("Truth Table for (A ∧ ¬(B ∧ C)")
 print("By Mitchell Kealy\n")
